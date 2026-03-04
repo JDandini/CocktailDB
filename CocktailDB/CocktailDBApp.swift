dd01b96eb36cabfecccce34c688120115f8f6a66
Javier Castañeda <javocst@yahoo.com.mx>
2026-03-04 06:57:10 -0600

    Initial Commit

---
+++ CocktailDB/CocktailDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailDBApp.swift
+//  CocktailDB
+//
+//  Created by Javier Castañeda on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CocktailDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
